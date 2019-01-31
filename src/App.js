3ac9cd48abe497392647553834223ac4de0f9228
Leonardo Chinem Kameya <leonardo.kameya@sanesul.ms.gov.br>
2019-01-31 10:48:45 -0300

    Setting up some tests with Enzyme and Sinon

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 const API = 'https://hn.algolia.com/api/v1/search?query=';
@@ -19,65 +19,56 @@ class App extends Component {
     super(props);
 
     this.state = {
-      counter: 0,
+      hits: [],
+      isLoading: false,
+      teste: false,
+      error: null,
     };
-
-    this.onIncrement = this.onIncrement.bind(this);
-    this.onDecrement = this.onDecrement.bind(this);
   }
 
-  onIncrement() {
-    this.setState(doIncrement);
-  }
-
-  onDecrement() {
-    this.setState(doDecrement);
+  getStories = async () => {
+    this.setState({ isLoading: true });
+    try {
+      const result = await axios.get(API + DEFAULT_QUERY)
+      console.log(result);
+      this.setState({
+        hits: result.data.hits,
+        isLoading: false
+      });
+    }
+    catch (e) {
+      this.setState({
+        error: e,
+        isLoading: false
+      })
+    }
   }
 
   componentDidMount() {
-    this.setState({
-      isLoading: true
-    });
-
-    fetch(API + DEFAULT_QUERY)
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error('Something went wrong');
-        }
-      })
-      .then(data => this.setState({ hits: data.hits, isLoading: false }))
-      .catch(error => this.setState({ error, isLoading: false }));
+    this.getStories();
   }
 
   render() {
-    const { counter } = this.state;
+    const { hits, isLoading, error } = this.state;
+
+    if (error) {
+      return <p>{error.message}</p>;
+    }
+
+    if (isLoading) {
+      return <p>Loading ...</p>;
+    }
 
     return (
-      <div>
-        <h1>My Counter</h1>
-        <Counter counter={counter} />
-
-        <button
-          type="button"
-          onClick={this.onIncrement}
-        >
-          Increment
-        </button>
-
-        <button
-          type="button"
-          onClick={this.onDecrement}
-        >
-          Decrement
-        </button>
-      </div>
+      <ul>
+        {hits.map(hit =>
+          <li key={hit.objectID}>
+            <a href={hit.url}>{hit.title}</a>
+          </li>
+        )}
+      </ul>
     );
   }
 }
-
-export const Counter = ({ counter }) =>
-  <p>{counter}</p>
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App, { doIncrement, doDecrement } from './App';
+import { shallow, mount, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import axios from 'axios';
+import sinon from 'sinon';
+
+configure({ adapter: new Adapter() });
 
 // Testing functions
 it('should increment the counter in state', () => {
@@ -17,10 +23,70 @@ it('should decrement the counter in state', () => {
   expect(newState.counter).toEqual(-1);
 });
 
-
 // Smoke test
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders without crashing', () => {
+  shallow(<App />);
+});
+
+describe('App', () => {
+
+  const result = {
+    data: {
+      hits: [
+        { objectID: '1', url: 'https://blog.com/hello', title: 'hello', },
+        { objectID: '2', url: 'https://blog.com/there', title: 'there', },
+      ],
+    }
+  };
+
+  const promise = Promise.resolve(result);
+
+  beforeAll(() => {
+    sinon
+      .stub(axios, 'get')
+      .withArgs('https://hn.algolia.com/api/v1/search?query=redux')
+      .returns(promise);
+  });
+
+  afterAll(() => {
+    axios.get.restore();
+  });
+
+  it('stores data in local state', (done) => {
+    const wrapper = mount(<App />);
+
+    expect(wrapper.state().hits).toEqual([]);
+
+    promise.then(() => {
+      wrapper.update();
+
+      expect(wrapper.state().hits).toEqual(result.data.hits);
+      expect(wrapper.state().teste).toEqual(false);
+
+      done();
+    });
+  });
+
+
+  it('renders data when it fetched data successfully', (done) => {
+    const wrapper = mount(<App />);
+
+    expect(wrapper.find('p').text()).toEqual('Loading ...');
+
+    promise.then(() => {
+      wrapper.update();
+
+      expect(wrapper.find('li')).toHaveLength(2);
+
+      done();
+    });
+  });
+
+});
+
