5997a2f6d01700d62a843e88ea791e5d42893386
Leonardo Chinem Kameya <leonardo.kameya@sanesul.ms.gov.br>
2019-01-31 09:01:09 -0300

    Adding some tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App, { doIncrement, doDecrement } from './App';
 
+// Testing functions
+it('should increment the counter in state', () => {
+  const state = { counter: 0 };
+  const newState = doIncrement(state);
+
+  expect(newState.counter).toEqual(1);
+});
+
+it('should decrement the counter in state', () => {
+  const state = { counter: 0 };
+  const newState = doDecrement(state);
+
+  expect(newState.counter).toEqual(-1);
+});
+
+
+// Smoke test
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
